e36c183ae79c9a79a8579abfae4283f075ef166d
Mark Dickson Jr <markdicksonjr@gmail.com>
2019-10-16 14:37:40 -0400

    UCS-2 XML handling added

--- xml/reader.go
+++ xml/reader.go
@@ -2,9 +2,11 @@ package xml
 
 import (
 	"encoding/xml"
+	"errors"
 	"golang.org/x/net/html/charset"
 	"io"
 	"os"
+	"strings"
 )
 
 // converts a file to records ((data, error) tuples)
@@ -52,8 +54,41 @@ func (r *Reader) Open(filename string) error {
 		return err
 	}
 
-	r.decoder = xml.NewDecoder(r.xmlFile)
-	r.decoder.CharsetReader = charset.NewReaderLabel
+	// read the first 64 characters to detect the encoding
+	first64 := make([]byte, 64)
+	if _, err = r.xmlFile.Read(first64); err != nil {
+		return err
+	}
+	_, name, _ := charset.DetermineEncoding(first64, "text/xml")
+	if _, err := r.xmlFile.Seek(0, 0); err != nil {
+		return err
+	}
+
+	// if no encoding could be determined, attempt to fall back to UTF-8
+	if name == "" {
+		name = "UTF-8"
+	}
+
+	// create a reader label for the encoding we assume the file is, falling back to a UTF-8 encoding
+	nr, _ := charset.NewReaderLabel(name, r.xmlFile)
+	if nr == nil {
+		nr, _ = charset.NewReaderLabel("UTF-8", r.xmlFile)
+	}
+
+	// if no reader label could be created, error out
+	if nr == nil {
+		return errors.New("could not allocate reader label")
+	}
+
+	r.decoder = xml.NewDecoder(nr)
+	r.decoder.CharsetReader = func(set string, input io.Reader) (reader io.Reader, e error) {
+
+		// html charset map in Go does not include UCS-2
+		if strings.ToLower(set) == "ucs-2" {
+			set = "utf-16le"
+		}
+		return charset.NewReaderLabel(set, input)
+	}
 
 	return nil
 }
